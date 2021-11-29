f90e73ab60b0d6c4f00295c99a7f5157a5fd8ca0
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-11-29 10:02:26 +0100

    inti

---
+++ src/main/java/de/tekup/vue/controllers/IndexCtrl.java
@@ -0,0 +1,19 @@
+package de.tekup.vue.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import de.tekup.vue.ui.models.WelcomeMsg;
+
+@Controller
+public class IndexCtrl {
+	
+	@GetMapping({"/","/index"})
+	public String getIndex(Model model) {
+		WelcomeMsg welcome = new WelcomeMsg("Welcome GLSI-C", "First Vue Project");
+		model.addAttribute("welcome", welcome);
+		return "index";
+	}
+
+}
